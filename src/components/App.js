2fb8114f79e61c3f23a5c5b94cb759e7aee43246
Pravendra Kumar <pravendraksj7.com>
2023-02-13 21:56:16 +0530

    complite

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
+
+  const [show, setShow] = useState(true);
+
+  function toggle(){
+    setShow(!show);
+    console.log(show);
+  }
+
   return (
     <div id="main">
-      <div id="welcome-div">
+  
+      {show ? <div id="welcome-div">
         <h1>Welcome to Newton School!!</h1>
       </div>
+      :
       <div id="about-div">
         <h1>
           Newton School is a neo-university providing highly immersive tech
           learning to millions of students, to tap into new-age tech
           opportunities.
         </h1>
-      </div>
-      <button id="toggle">Toggle</button>
+      </div>}
+      <button id="toggle" onClick={toggle}>Toggle</button>
     </div>
   );
 };
